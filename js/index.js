be250a21a706741e79fa4e847345fc664003eec1
Tatiana S <officialemail.cc@gmail.com>
2020-02-29 19:30:11 +0500

    lesser js and css fixes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
-function getWindowWidth() {
-  return window.innerWidth || document.body.clientWidth;
-}
 
-if (getWindowWidth() < 769) {
   function showMenu() {
     document.getElementById('topmenu').classList.toggle("menu__open");
   }
-}
